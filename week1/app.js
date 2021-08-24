7e236f582bdd5a8eb6a96ceef5eb5a4123e1fc54
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2021-08-24 17:53:40 -0400

    first commit

---
+++ week1/app.js
@@ -0,0 +1,7 @@
+const math = require('./math');
+
+console.log(math)
+console.log(math.sum(10,2));
+console.log(math.subtract(10,2));
+console.log(math.divide(10,2));
+console.log(math.multiply(10,2));
